3fbd7a6a012d38e71791ae8d95a08bb63a7033ca
Joerg Schmuecker <j.schmuec@mongodb.com>
2022-09-25 07:18:47 +0200

    added test

--- src/bitemp.test.ts
+++ src/bitemp.test.ts
@@ -30,45 +30,48 @@ const latest = (d: Doc, { event_time = undefined, system_cut_off = undefined }: 
     }, undefined);
 
 describe("bitemporal overlay", () => {
-    var doc: Doc;
 
-    beforeEach(() => {
-        doc = [] as Doc;
+    let events: { ingestion_time: number, event_time: number }[] =
+        [{ ingestion_time: 0, event_time: 0 },
+        { ingestion_time: 100, event_time: 100 },
+        { ingestion_time: 200, event_time: 50 },
+        { ingestion_time: 300, event_time: 100 },
+        { ingestion_time: 400, event_time: 200 },
+        ];
 
-        let events: { ingestion_time: number, event_time: number }[] =
-            [{ ingestion_time: 0, event_time: 0 },
-            { ingestion_time: 100, event_time: 100 },
-            { ingestion_time: 200, event_time: 50 },
-            { ingestion_time: 300, event_time: 100 },
-            { ingestion_time: 400, event_time: 200 },
-            ];
+    let doc = events.reduce((doc, evt) => observe(doc, evt.ingestion_time, `s: ${evt.ingestion_time}, e: ${evt.event_time}`, evt.event_time), [] as Doc )
 
-        doc = events.reduce((doc, evt) => observe(doc, evt.ingestion_time, `s: ${evt.ingestion_time}, e: ${evt.event_time}`, evt.event_time), doc)
+    describe("picking the rights version", () => {
+        test("latest() should return the observation with the highest event and system time", () => {
+            expect(latest(doc)?.evt).toEqual("s: 400, e: 200");
+        })
+
+        test("get the observation that happend before 50", () => {
+            expect(latest(doc, { system_cut_off: 50 })?.evt).toEqual("s: 0, e: 0");
+        })
+
+        test("get the state as per event_time 75", () => {
+            expect(latest(doc, { event_time: 75 })?.evt).toEqual("s: 200, e: 50");
+        })
+
+        test("get the state as per system_time 125 and event_time 125", () => {
+            expect(latest(doc, { event_time: 125, system_cut_off: 125 })?.evt).toEqual("s: 100, e: 100");
+        }
+        );
+
+        test("get the state as per system_time 300 and event_time 125", () => {
+            expect(latest(doc, { event_time: 125, system_cut_off: 300 })?.evt).toEqual("s: 300, e: 100");
+        }
+        );
     })
 
-    test("latest() should return the observation with the highest event and system time", () => {
-        expect(latest(doc)?.evt).toEqual("s: 400, e: 200");
+    type database<K, V> = { get: (ps: { id: K, ingestion_time?: number, event_time?: number, filter?: (v: V) => boolean }) => V }
+
+    const latest_matching = ( ps : { doc : Doc, matches : Doc } ) => doc[0]
+
+    describe("Testing with filters", () => {
+        test("if all versions match, get the latest", () => {
+            expect( latest_matching( { doc, matches : doc })?.evt ).toEqual( latest( doc )?.evt )
+        })
     })
-
-    test("get the observation that happend before 50", () => {
-        expect(latest(doc, { system_cut_off: 50 })?.evt).toEqual("s: 0, e: 0");
-    })
-
-    test("get the state as per event_time 75", () => {
-        expect(latest(doc, { event_time: 75 })?.evt).toEqual("s: 200, e: 50");
-    })
-
-    test("get the state as per system_time 125 and event_time 125", () => {
-        expect(latest(doc, { event_time: 125, system_cut_off: 125 })?.evt).toEqual("s: 100, e: 100");
-    }
-    );
-
-    test("get the state as per system_time 300 and event_time 125", () => {
-        expect(latest(doc, { event_time: 125, system_cut_off: 300 })?.evt).toEqual("s: 300, e: 100");
-    }
-    );
-})
-
-describe("Testing with filters", () => {
-    it.todo("Push filter into aggregation pipeline");
 })
